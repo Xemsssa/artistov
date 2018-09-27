663eebe88b5cac90905422aac6e9fd412cff7b1c
Xemsssa <Xemsss@gmail.com>
2018-09-27 12:47:19 +0300

    zzz

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
+        // lazyLoad:true,
+        items:1,
+        autoplay: true,
+        smartSpeed: 15000,
         loop: true,
         margin: 10,
         responsiveClass: true,
@@ -9,11 +13,11 @@ $(document).ready(function () {
                 nav: true,
             },
             600: {
-                items: 3,
+                items: 2,
                 nav: false
             },
             1000: {
-                items: 5,
+                items: 6,
                 nav: true,
                 loop: false
             }
